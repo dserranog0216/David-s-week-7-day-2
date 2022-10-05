6932e341731de4ba75bcd50bcbcd02f173d54a2b
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-10-05 19:19:02 +0000

    Loops day 2 challenge 3

--- loops.py
+++ loops.py
@@ -141,9 +141,24 @@ def loop():
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
   
-  # sum_even = 
+  # sum_even = 0
   
-  # sum_odd = 
+  # sum_odd = 0
+
+  # for num in list_numbers:
+  #   if num % 2 == 0:
+  #     sum_even = sum_even +num
+  #   elif num % 2 == 1:
+  #     sum_odd = sum_odd +num
+  # print(f"Evens are {sum_even}")
+  # print(f"Odds are {sum_odd}")
+
+
+
+
+
+
+  
 
 
 
