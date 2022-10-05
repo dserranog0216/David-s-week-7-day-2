f01c85d40d436f3f7432734ac935f7958ce5b1ac
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-10-05 19:06:02 +0000

    Loops  practice 1 AND 2 DAY 2

--- loops.py
+++ loops.py
@@ -108,14 +108,21 @@ def loop():
   
   students = ["Norville", "Fred", "Velma", "Daphne"]
   
-  
+  for name in students:
+    print(f"hello {name}")
   
   
   # For Loops Practice #2
   # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  # sum_numbers = 
+  sum_numbers = 0
+  for number in list_numbers:
+    sum_numbers = sum_numbers + number
+    print(f"Sum of list_numbers is {sum_numbers}")
+
+
+  
   
   
   
