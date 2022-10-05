b5ab9df46083be13000fe946db421c363b17b354
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-10-05 19:42:48 +0000

    Finished ranges

--- ranges.py
+++ ranges.py
@@ -35,11 +35,14 @@ def ranges():
   # Range Practice #1
   # Create a list consisting of all the numbers from 2500 to 2585 (inclusive). Store this list in the variable my_list.
   
-  
+  my_list = list(range(2500,2586))
+  print(my_list)
   
   # Range Practice #2
   # Using the range() function, create in a single line of code a list consisting of all numbers that are multiples of 3 from 3 to 300 (inclusive). Store this list in the variable my_list.
-  
+
+  my_list = list(range(3,301,3))
+  print(my_list)
   
   
   # Range Practice #3
@@ -55,4 +58,18 @@ def ranges():
   
   # Sum all the squared values obtained. Accumulate the sum in the variable sum_squares.
   
+  import math
+  sum_squares = 0
+  for i in range(250):
+    sum_squares = sum_squares + i**2
+  print(sum_squares)
+
+  for i in range(20,291):
+    print(i)
   
+  
+
+
+
+
+
